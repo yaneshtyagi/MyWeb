786caba264aef19e0851276a92f2d55409a25ae2
yaneshtyagi <yanesh.tyagi@gmail.com>
2014-08-18 06:56:48 +0530

    Added version number and other minor improvements

--- yaneshtyagi.net/Properties/AssemblyInfo.cs
+++ yaneshtyagi.net/Properties/AssemblyInfo.cs
@@ -1,17 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("yaneshtyagi.net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("yaneshtyagi.net website assembly")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yanesh Tyagi")]
 [assembly: AssemblyProduct("yaneshtyagi.net")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Yanesh Tyagi")]
+[assembly: AssemblyTrademark("Yanesh Tyagi")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -31,5 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.25.*")]
+//[assembly: AssemblyFileVersion("2.2.25.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
